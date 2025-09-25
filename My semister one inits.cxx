c83dde594478df0f6920a10b00c49c813d2e15a4
jeffmwandikwa254 <jeffmwandikwa254@gmail.com>
2025-09-25 21:30:53 +0300

    Add files via upload
    My semister one inits.cxx

---
+++ My semister one inits.cxx
@@ -0,0 +1,26 @@
+/*MY COMPUTER SCIENCE UNITS FOR SEMISTER ONE;
+Name:Jeff Mwandikwa
+Reg.No:CT101/G/26533/25
+Course:Computer science'
+
+UCU2105 Fundamental of ICT/n;
+SPC2102 Introduction to computer architecture/n;
+UCU2101 Communication skills/n;
+SPH2107 Analogue electronics/n;
+SPM2100 Discreate mathematics;
+SPM2104 Mathematics for science/n;
+SPH2101 Physics/n;
+SPC2101 Introduction to programming and problem solving/n;
+*/
+#include <stdio.h>
+int main(){
+ printf("Fundamentals of ICT\n");
+ printf("Introduction to computer artch\n");
+ printf("Communication skills\n"); 
+ printf("Analogue electronics\n");
+ printf("Discreate mathematics\n");
+ printf("Mathematics for sciience\n");
+ printf("Physics\n");
+ printf("Introduction to programming and problem solvimg\n ");
+ return 0;
+}
